4f209aedaf289be8d3c31534043c7f234cb55cef
Akshitha296 <70026624+Akshitha296@users.noreply.github.com>
2020-12-07 22:17:08 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+function setup() {
+  createCanvas(480,800);
+
+  ground = new Ground(240, 470, 800, 10)
+}
+
+function draw() {
+  background(0);  
+}
